8037f90839769c2553fa9f0eb345126ab386a172
Moumili Halder <moumilihalder@gmail.com>
2026-03-16 12:29:44 +0530

    change files

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,26 +1,20 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization; // check this
 
-  const token = req.headers.authorization;
-
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token" });
   }
 
+  const token = authHeader.split(" ")[1]; // get token only
+
   try {
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     req.userId = decoded.id;
-
     next();
-
   } catch (error) {
-
     res.status(401).json({ message: "Invalid token" });
-
   }
 };
-
 export default authMiddleware;
